905d8e1e3d7189d59ee3f8eb4fe78106f299ee53
liangxiao05 <liangxiao06@meituan.com>
2020-05-12 00:33:30 +0800

    接入swagger

---
+++ fb-web/src/main/java/com/fb/web/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.fb.web.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(value = "swagger.enable", havingValue = "true")
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fb.web.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        // 填写文档开发者信息
+//        Contact contact = new Contact("3795", "https://www.ntshare.cn", "seven.iwi@gmail.com");
+        return new ApiInfoBuilder()
+                .title("T-bear项目组")
+                .description("接口文档")
+//                .contact(contact)
+                .version("1.0.0")
+                .build();
+    }
+}
+
--- fb-web/src/main/java/com/fb/web/controller/TestController.java
+++ fb-web/src/main/java/com/fb/web/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.fb.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
+@Api("探熊")
 public class TestController {
 
+    @ResponseBody
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @ApiOperation(value = "探熊", notes = "探熊api文档")
+    public String hello(@ApiParam(name = "say", value = "说点啥吧") @RequestParam("say") String say) {
+        return say;
     }
 }
