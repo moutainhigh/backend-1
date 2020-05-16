86d9f10344bdd1ad20738b07fbe1a77c30a3c5db
liangxiao <liangxiao199207@163.com>
2020-05-17 01:49:23 +0800

    接入mybatis-plus

--- fb-activty/src/main/java/com/fb/activty/dao/DeptDao.java
+++ fb-activty/src/main/java/com/fb/activty/dao/DeptDao.java
@@ -2,7 +2,6 @@ package com.fb.activty.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.fb.activty.entity.Dept;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
